12fa29293d70c9a813f9624534fa069cef5c6c86
Donovancm <Donovan@DESKTOP-M98EOT9>
2017-02-28 11:04:37 +0100

    Add project files.

---
+++ InfomationManagementCendrisWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InfomationManagementCendrisWebApp.Startup))]
+namespace InfomationManagementCendrisWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
