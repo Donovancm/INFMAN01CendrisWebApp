a33a2a8145598419b853382e68f7d25162c9b5ea
Michel <michelv1994@caiway.net>
2017-03-01 19:36:54 +0100

    Toevoeging controller en bijhorende pagina's

---
+++ InfomationManagementCendrisWebApp/Controllers/VrijwilligerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfomationManagementCendrisWebApp.Controllers
+{
+    public class VrijwilligerController : Controller
+    {
+        // GET: /Vrijwilliger/
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Vrijwilliger";
+
+            return View();
+        }
+
+        // GET: /Vrijwilliger/Dashboard
+        public ActionResult Dashboard()
+        {
+            ViewBag.Message = "Dashboard";
+
+            return View();
+        }
+    }
+}
+
